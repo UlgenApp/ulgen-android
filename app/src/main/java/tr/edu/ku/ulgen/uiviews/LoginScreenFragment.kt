c2ea83036c6573f59130b0be94730939bb0302ab
aybarsinci <aybarsinci00@gmail.com>
2023-04-21 04:13:20 +0300

    Bottom bar navigation implemented.

--- app/src/main/java/tr/edu/ku/ulgen/uiviews/LoginScreenFragment.kt
+++ app/src/main/java/tr/edu/ku/ulgen/uiviews/LoginScreenFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -21,6 +22,11 @@ class LoginScreenFragment : Fragment() {
         val resetPasswordTextView = view.findViewById<TextView>(R.id.txtIfrenimiunut)
         resetPasswordTextView.setOnClickListener {
             findNavController().navigate(R.id.action_loginScreenFragment_to_resetPasswordFragment)
+        }
+
+        val loginButton = view.findViewById<Button>(R.id.btnGiriYap)
+        loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginScreenFragment_to_kandilliTableFragment)
         }
 
         return view
