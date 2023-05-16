74986a17fda0876eee105cdd9fca734920683ccc
aybarsinci <aybarsinci00@gmail.com>
2023-05-16 22:24:47 +0300

    reset screens UI updated.

--- app/src/main/java/tr/edu/ku/ulgen/uiviews/HomeScreenFragment.kt
+++ app/src/main/java/tr/edu/ku/ulgen/uiviews/HomeScreenFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import tr.edu.ku.ulgen.R
+import tr.edu.ku.ulgen.model.SharedPreferencesUtil
 
 class HomeScreenFragment : Fragment() {
+
+    private lateinit var sharedPreferencesUtil: SharedPreferencesUtil
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +20,11 @@ class HomeScreenFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home_screen, container, false)
 
+        sharedPreferencesUtil = SharedPreferencesUtil(requireContext())
+
+        val nameText = view.findViewById<TextView>(R.id.txtLanguage)
+
+        nameText.text = sharedPreferencesUtil.getUserProfile()?.firstName.toString()
 
 
 
