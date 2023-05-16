06c89ed89376b49d932ab456d17fb8abe4457a31
aybarsinci <aybarsinci00@gmail.com>
2023-05-16 21:02:28 +0300

    start, login, signup screens UI updated.

--- app/src/main/java/tr/edu/ku/ulgen/uiviews/ResetPasswordFragment.kt
+++ app/src/main/java/tr/edu/ku/ulgen/uiviews/ResetPasswordFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import tr.edu.ku.ulgen.R
 
 class ResetPasswordFragment : Fragment() {
@@ -16,6 +19,17 @@ class ResetPasswordFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.activity_reset_password_screen, container, false)
 
+        val resetPasswordButton = view.findViewById<Button>(R.id.btnBalantyGnder)
+
+        val loginText = view.findViewById<TextView>(R.id.txtGiriyap)
+
+        loginText.setOnClickListener {
+            findNavController().navigate(R.id.action_resetPasswordFragment_to_loginScreenFragment)
+        }
+
+        resetPasswordButton.setOnClickListener {
+
+        }
 
 
         return view
